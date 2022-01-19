003bf826a975721af10c1a743e8900c8c279f221
Hadar <hadar.cohen147@gmail.com>
2022-01-19 16:57:53 +0200

    add ClientSocket Abstraction

---
+++ ConsoleApp3/Client/SocketImplement/Abstract/IClientSocket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong.Server
+{
+    public interface IClientSocket
+    {
+        public void Connect();
+        public byte[] Receive();
+        public void Send(byte[] data);
+        public void Close();
+    }
+}

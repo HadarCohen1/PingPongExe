71cf25717a414b0a2662d416183a2fef24b70082
Hadar <hadar.cohen147@gmail.com>
2022-01-19 17:01:34 +0200

    add ClientSyncSocket class

--- ConsoleApp3/Client/SocketImplement/Abstract/IClientSocket.cs
+++ ConsoleApp3/Client/SocketImplement/Abstract/IClientSocket.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PingPong.Server
+namespace PingPong.Client.SocketImplement.Abstract
 {
     public interface IClientSocket
     {
--- ConsoleApp3/Client/SocketImplement/Implemention/ClientSyncSocket.cs
+++ ConsoleApp3/Client/SocketImplement/Implemention/ClientSyncSocket.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PingPong.Client.SocketImplement.Abstract;
 
 namespace PingPong.Client.SocketImplement.Implemention
 {
-    public class ClientSyncSocket
+    public class ClientSyncSocket : IClientSocket
     {
+        public void Connect()
+        {
+            
+        }
+
+        public byte[] Receive()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

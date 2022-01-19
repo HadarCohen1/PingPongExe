49102363dc08a1ffa9a32cb17f00b8c35dd04da3
HadarCohen1 <98028760+HadarCohen1@users.noreply.github.com>
2022-01-19 17:35:02 +0200

    add Input Abstraction

--- ConsoleApp3/Client/Implemention/TcpClient.cs
+++ ConsoleApp3/Client/Implemention/TcpClient.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PingPong.Client.SocketImplement.Abstract;
+using PingPong.Output.Abstract;
+using PingPong.Input.Abstract;
 
 namespace PingPong.Client.Implemention
 {
-    public class TcpClient
+    public class TcpClient<T> : IClient
     {
+        public IClientSocket ClientSocket;
+        public IInput<string> Reader;
+        public IOutput<string> Printer;
+
+        public TcpClient(IClientSocket clientSocket, IInput<string> reader, IOutput<string> printer)
+        {
+            ClientSocket = clientSocket;
+            Reader = reader;
+            Printer = printer;
+        }
+
+        public void StartClient()
+        {
+            ClientSocket.Connect();
+            while(true)
+            {
+                ClientSocket.Send(Encoding.ASCII.GetBytes(Reader.Read()));
+                Printer.Print(ClientSocket.Receive());
+            }
+        }
     }
 }
---
+++ ConsoleApp3/Output/Implemention/ConsoleOutput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PingPong.Output.Abstract;
+
+namespace PingPong.Output.Implemention
+{
+    public class ConsoleOutput : IOutput<string>
+    {
+        public void Print(string toPrint)
+        {
+            Console.WriteLine(toPrint);
+        }
+    }
+}
